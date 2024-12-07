b39c90595b499f38c09ae3bcb506f178334b10d2
Anna shabbagh <afreenshabbagh@gmail.com>
2024-12-07 14:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function handleLogin(){
+	const username = document.getElementById("username").value;
+	const password = document.getElementById("password").value;
+	const checkbox = document.getElementByID('checkbox').checked;
+
+	if(checbox){
+		localStorage.setItem('username', username);
+		localStorage.setItem('password', password)
+	}else{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");
+	}
+	alert("Logged is as " + username);
+
+	const savedUsername = localStorage.getItem("username");
+     if (savedUsername) {
+    document.getElementById("existing").style.display = "block";
+  }
+}
+function handleExistingLogin() {
+  const savedUsername = localStorage.getItem("username");
+  alert("Logged in as " + savedUsername);
+}
